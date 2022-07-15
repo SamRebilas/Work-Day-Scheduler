56f33cb4fab1c8f22476b328e3ef8ea9bca269f5
Samrebilas <sam.rebilas@gmail.com>
2022-07-15 10:14:01 -0400

    fresh start

---
+++ script.js
@@ -0,0 +1,23 @@
+ //date at top of screen
+
+
+
+ $(document).ready(function(){
+    $("#date").text( moment().format('ddd MMM Do, YYYY'))
+}
+)
+//text editor
+$(".time-block").on("click", "p", function() {
+   
+  });
+
+
+
+  
+   
+ 
+   
+   
+    
+
+
