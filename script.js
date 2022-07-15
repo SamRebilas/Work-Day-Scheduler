1f0124c20b748c7f9e1817072ac17d94ba511d67
Samrebilas <sam.rebilas@gmail.com>
2022-07-15 10:36:53 -0400

    timer is functioning properly

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
  //date at top of screen
-
-
-
- $(document).ready(function(){
+$(document).ready(function(){
     $("#date").text( moment().format('ddd MMM Do, YYYY'))
-}
-)
+})
 //text editor
-$(".time-block").on("click", "p", function() {
-   
-  });
 
+
+function timeInfo (){
+$(".userText").each(function(){
+var hour = $(this).parent().attr("id")
+var timeNow = moment().hour();
+
+if (hour < timeNow){
+$(this).addClass("past")
+}
+else if (hour == timeNow){
+  $(this).addClass("present")
+}
+else if(hour > timeNow) {
+  $(this).addClass("future")
+}
+})
 
 
   
-   
+}
  
-   
+timeInfo()
    
     
 
