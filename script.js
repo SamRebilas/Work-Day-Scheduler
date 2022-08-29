8ffc41e5f0d6278b303bc4e1e16a8b1256ee7768
Samrebilas <sam.rebilas@gmail.com>
2022-08-29 15:23:21 -0400

    updated save, added itemEl, and editItems

--- script.js
+++ script.js
@@ -3,40 +3,46 @@ $(document).ready(function(){
     $("#date").text( moment().format('ddd MMM Do, YYYY'))
 })
 //text editor
+var itemEl = function(){
+  $(".time-block").each(function (){
+    var hour = $(this).attr("id")
+    var text = localStorage.getItem(hour)
+    $(this).children(".content").text(text)
+  })
+}
+var save = function (e){
+  var text = $(e.target).prev().text().trim()
+  var time = $(e.target).parent().attr("id")
+  localStorage.setItem(time, text);
+}
+var editItem = function (e) {
+  var text = $(this).text()
+  var input = $("input").addClass("col-1").val(text)
+  $(this).replaceWith(input)
+};
+$("body").on("click", ".saveBtn", save)
+$("body").on("click", ".content", editItem)
+itemEl();
 
 
-function timeInfo (){
+var timeEl = function (){
 $(".userText").each(function(){
 var hour = $(this).parent().attr("id")
-var timeNow = moment().hour();
-
+var timeNow = moment().hour()
+$(this).removeClass("past present future")
 if (hour < timeNow){
-$(this).addClass("past")
+$(this).addClass(".past")
 }
 else if (hour == timeNow){
-  $(this).addClass("present")
+ $(this).addClass(".present")
 }
 else if(hour > timeNow) {
-  $(this).addClass("future")
+ $(this).addClass(".future")
 }
 })
-
-
-  
 }
  
 
-   
- function saveText  (){
-  textEl = (document.getElementById("userText").value)
-  localStorage.setItem("userText", textEl)
-  console.log(textEl)
- }
 
-
-document.querySelector(".saveBtn").addEventListener("click", function(){
-  saveText()
-})
-
-
-timeInfo()
+itemEl()
+timeEl()
